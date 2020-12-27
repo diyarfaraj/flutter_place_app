8c49b5993bfade2929af92217dfe12f94d10d93b
diyarfaraj <diyar.faraj@gmail.com>
2020-12-27 14:51:18 +0100

    git init

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add new place"),
+      ),
+    );
+  }
+}
